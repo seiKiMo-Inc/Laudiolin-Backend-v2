35f48a8a558f36e6a84e2e827b5d0c3ed421a125
KingRainbow44 <kobedo11@gmail.com>
2023-08-20 14:50:01 -0400

    Add a different use-case for the seek command

--- src/main/java/moe/seikimo/laudiolin/Command.java
+++ src/main/java/moe/seikimo/laudiolin/Command.java
@@ -218,13 +218,6 @@ public final class Command {
                 logger.info("Skipped track.");
             }
             case "seek" -> {
-                // Check if a position was specified.
-                if (args.isEmpty()) {
-                    logger.info("No arguments provided.");
-                    logger.info("Usage: /elixir seek <position>");
-                    return;
-                }
-
                 // Check for a target guild.
                 if (Command.targetGuildId == null) {
                     logger.info("No target guild ID set.");
@@ -239,16 +232,23 @@ public final class Command {
                     return;
                 }
 
-                // Parse the position.
-                var position = Float.parseFloat(args.get(0));
-                if (position < 0) {
-                    logger.info("Position must be greater than 0.");
-                    return;
-                }
+                // Check if a position was specified.
+                if (args.isEmpty()) {
+                    // Get the current position.
+                    var position = guild.getTrackPosition();
+                    logger.info("Current position is {}.", position);
+                } else {
+                    // Parse the position.
+                    var position = Float.parseFloat(args.get(0));
+                    if (position < 0) {
+                        logger.info("Position must be greater than 0.");
+                        return;
+                    }
 
-                // Seek to the position.
-                ElixirUtils.seek(guild, (long) (position * 1000f));
-                logger.info("Seeked to position {}.", position);
+                    // Seek to the position.
+                    ElixirUtils.seek(guild, (long) (position * 1000f));
+                    logger.info("Seeked to position {}.", position);
+                }
             }
         }
     }
