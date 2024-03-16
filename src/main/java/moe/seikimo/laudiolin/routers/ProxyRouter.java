496e81052bf631a724a406802c954624a5f70456
KingRainbow44 <kobedo11@gmail.com>
2024-03-16 13:52:33 -0400

    (feat:security) Proxy icon URLs

--- src/main/java/moe/seikimo/laudiolin/routers/ProxyRouter.java
+++ src/main/java/moe/seikimo/laudiolin/routers/ProxyRouter.java
@@ -3,8 +3,13 @@ package moe.seikimo.laudiolin.routers;
 import io.javalin.Javalin;
 import io.javalin.http.ContentType;
 import io.javalin.http.Context;
+import moe.seikimo.laudiolin.utils.EncodingUtils;
 import moe.seikimo.laudiolin.utils.HttpUtils;
+import moe.seikimo.laudiolin.utils.NetUtils;
 
+import java.util.Base64;
+
+import static moe.seikimo.laudiolin.utils.HttpUtils.INTERNAL_ERROR;
 import static moe.seikimo.laudiolin.utils.HttpUtils.INVALID_ARGUMENTS;
 
 public interface ProxyRouter {
@@ -14,7 +19,44 @@ public interface ProxyRouter {
      * @param javalin The Javalin instance.
      */
     static void configure(Javalin javalin) {
+        javalin.get("/proxy/icon", ProxyRouter::iconProxy);
         javalin.get("/proxy/{arg}", ProxyRouter::proxy);
+    }
+
+    /**
+     * Handles requests for external image proxies.
+     * This handles requests for playlist and user icons.
+     *
+     * @param ctx The context.
+     */
+    static void iconProxy(Context ctx) {
+        try {
+            // Pull arguments.
+            var url = ctx.queryParam("url");
+
+            // Check if the arguments are valid.
+            if (url == null || url.isEmpty()) {
+                ctx.status(400).json(INVALID_ARGUMENTS());
+                return;
+            }
+
+            // Attempt to Base64 decode the URL.
+            var decodedUrl = Base64.getUrlDecoder().decode(url);
+            url = new String(decodedUrl);
+
+            // Validate the URL.
+            if (!NetUtils.validUrl(url)) {
+                ctx.status(400).json(INVALID_ARGUMENTS("Invalid URL provided."));
+                return;
+            }
+
+            // Make the request.
+            ctx.status(200).result(HttpUtils.makeRequest(url))
+                    .contentType(ContentType.IMAGE_JPEG)
+                    .header("Cache-Control", "public, max-age=604800, immutable");
+        } catch (Exception exception) {
+            ctx.status(500).json(INTERNAL_ERROR(exception.getMessage()));
+        }
     }
 
     /**
--- src/main/java/moe/seikimo/laudiolin/utils/NetUtils.java
+++ src/main/java/moe/seikimo/laudiolin/utils/NetUtils.java
@@ -2,6 +2,7 @@ package moe.seikimo.laudiolin.utils;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.URL;
 
 public interface NetUtils {
     /**
@@ -12,6 +13,23 @@ public interface NetUtils {
             return socket.getLocalPort();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Validates a given URL.
+     *
+     * @param url The URL to validate.
+     * @return Whether the URL is valid.
+     */
+    static boolean validUrl(String url) {
+        if (!url.startsWith("http")) return false;
+
+        try {
+            new URL(url).toURI();
+            return true;
+        } catch (Exception ignored) {
+            return false;
         }
     }
 }
