2e27e9376325da5ed6e48d227e0a4edfeb4376c0
KingRainbow44 <kobedo11@gmail.com>
2024-08-07 23:08:20 -0400

    feat(index.ts): Add support for OAuth2/credentials

--- src/node/src/index.ts
+++ src/node/src/index.ts
@@ -1,4 +1,6 @@
-import Innertube from "youtubei.js";
+import { writeFileSync, readFileSync, existsSync } from "node:fs";
+
+import Innertube, { UniversalCache } from "youtubei.js";
 import { Music } from "youtubei.js/dist/src/core/clients";
 
 import { Logger } from "tslog";
@@ -26,6 +28,33 @@ export const storagePath = process.env["STORAGE_PATH"] ?? `${process.cwd()}/file
 export let youtube: Innertube;
 export let ytMusic: Music;
 (async () => {
-    youtube = await Innertube.create();
+    youtube = await Innertube.create({
+        cache: new UniversalCache(true, storagePath)
+    });
     ytMusic = youtube.music;
+
+    // Prepare to do YouTube authentication.
+    youtube.session.on("auth-pending", (data) => {
+        logger.info(`Go to ${data.verification_url} in your browser and enter the code ${data.user_code} to authenticate.`)
+    });
+    youtube.session.on("auth", ({ credentials }) => {
+        logger.info("Successfully authenticated.");
+
+        // Write the credentials to the cache.
+        writeFileSync("credentials.json", JSON.stringify(credentials));
+    });
+    youtube.session.on("update-credentials", async ({ credentials }) => {
+        await youtube.session.oauth.cacheCredentials();
+
+        // Write the credentials to the cache.
+        writeFileSync("credentials.json", JSON.stringify(credentials));
+    });
+
+    // Do the authentication.
+    let credentials: any | undefined = undefined;
+    if (existsSync("credentials.json")) {
+        credentials = JSON.parse(readFileSync("credentials.json", "utf-8"));
+    }
+
+    await youtube.session.signIn(credentials);
 })();
